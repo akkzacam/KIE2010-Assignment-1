05ec9acfb3984c6f3e88deec1d9fd3016cad7215
Aydan <akkzacam@yahoo.com>
2026-04-26 10:48:24 +0800

    comments 3_2

--- 3.2/program.asm
+++ 3.2/program.asm
@@ -12,7 +12,7 @@ __Vectors
 		
 	area	mydata, data, readonly
 		
-test_data
+test_data		;stores different ascii values for different test cases
 	dcb 0x35, 0x37
 	dcb 0x39, 0x31
 	dcb 0x37, 0x37
@@ -23,7 +23,7 @@ test_data
 	export	Reset_Handler
 	
 Reset_Handler
-	mov r0, #3
+	mov r0, #3					;r0 = 3 for 3 diffeernt test cases
 	bl main
 stop
 	b stop
@@ -31,24 +31,24 @@ stop
 main
 	push {lr}
 	
-	ldr r1, =0x20000000	;store tens
-	ldr r2, =0x20000010	;store ones
-	ldr r3, =0x20000020	;store data
-	ldr r4, =test_data
+	ldr r1, =0x20000000			;store tens
+	ldr r2, =0x20000010			;store ones
+	ldr r3, =0x20000020			;store data
+	ldr r4, =test_data			;iterates through different values of testdata
 	
 loop_tests
-	ldrb r5, [r4]
-	add r4, #0x01
-	ldrb r6, [r4]
-	add r4, #0x01
+	ldrb r5, [r4]				;0x35 is loaded on r5
+	add r4, #0x01				;r4's position moved up by one byte
+	ldrb r6, [r4]				;0x37 is loaded onto r6
+	add r4, #0x01				;r4's position moved up by one byte
 	
-	bl test_logic
+	bl test_logic				;branches to test_logic function
 	
-	strb r5, [r3]
-	add r3, #1
+	strb r5, [r3]				; r5 (0101 0111) is stored in r3
+	add r3, #1					
 	add r1, #0x01
 	add r2, #0x01
-	sub r0, #1
+	sub r0, #1					;r0 subtracted with 1 to progress loop
 	cmp r0, #0
 	bne loop_tests
 	
@@ -56,16 +56,16 @@ loop_tests
 	bx lr
 	
 test_logic
-	mov r7, #0x30
-	sub r5, r5, r7	;5, 0000 0101
-	sub r6, r6, r7	;7, 0000 
+	mov r7, #0x30			;r7 stores 0x30
+	sub r5, r5, r7			;r5 = 5, 0000 0101
+	sub r6, r6, r7			;r7 = 7, 0000 0111
 	
-	strb r5, [r1]
-	strb r6, [r2]
+	strb r5, [r1]			;r5 is stored in r1
+	strb r6, [r2]			;r6 is stored in r2
 	
-	lsl r5, #0x04	;shift 0000 0101 to 0101 0000
-	orr r5, r5, r6	; logic or 0101 0000 with 0000 0111
+	lsl r5, #0x04			;shift 0000 0101 to 0101 0000
+	orr r5, r5, r6			; logic or 0101 0000 with 0000 0111 to get 0101 0111
 	
-	bx lr
+	bx lr					;brances back to main
 	
 	end
