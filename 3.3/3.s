c0854470e372307b9b714055f011d45fca8739c6
Aydan <akkzacam@yahoo.com>
2026-04-26 10:53:20 +0800

    comments 3_3

--- 3.3/3.s
+++ 3.3/3.s
@@ -11,7 +11,7 @@ __Vectors
 	align
 		
 	area mydata, data, readonly
-test_data
+test_data		;test data stores 3 different test values
 	dcb 47
 	dcb 92
 	dcb 57
@@ -21,35 +21,35 @@ test_data
 	entry
 	export Reset_Handler
 Reset_Handler
-	mov r0, #3
+	mov r0, #3		;r0 starts at 3 for 3 different test cases
 	bl main
 stop
 	b stop
 	
 main
 	push{lr}
-	ldr r1, =test_data	;loads test data in register 	
-	ldr r2, =0x20000010	;stores bcd code for ones
-	ldr r3, =0x20000020	;stores bcd code for tens
-	ldr r4, =0x20000030	;stores ascii code for ones
-	ldr r5, =0x20000040	;stores ascii code for tens
+	ldr r1, =test_data			;loads test data in register 	
+	ldr r2, =0x20000010			;stores bcd code for ones
+	ldr r3, =0x20000020			;stores bcd code for tens
+	ldr r4, =0x20000030			;stores ascii code for ones
+	ldr r5, =0x20000040			;stores ascii code for tens
 	
 loop_test
-	ldrb r12, [r1], #1
+	ldrb r12, [r1], #1			;47 is loaded onto r12
 	
-	bl test_logic
+	bl test_logic				;test_logic branch is called
 	
-	strb r9, [r2]
-	strb r7, [r3]
-	strb r12, [r4]
-	strb r11, [r5]
+	strb r9, [r2]				;r2 stores value of r9 = 7
+	strb r7, [r3]				;r3 stores value of r7 = 4
+	strb r12, [r4]				;r4 stores value of r12 = 0x37
+	strb r11, [r5]				;r5 stores value of r11 = 0x34
 	
-	add r2, #0x01
+	add r2, #0x01				;moves all registers by one byte for next test case iteration
 	add r3, #0x01
 	add r4, #0x01
 	add r5, #0x01
 	
-	sub r0, r0, #1
+	sub r0, r0, #1				;r0 subtracted wtih 1 until reaches 0 to terminate program
 	cmp r0, #0
 	bne loop_test
 	
@@ -57,15 +57,15 @@ loop_test
 	bx lr
 	
 test_logic
-	mov r6, #10	;stores decimal 10 into r4
+	mov r6, #10			;stores decimal 10 into r4
 	udiv r7, r12, r6	;r7 = 47 / 10 = 4
-	mul r8, r7, r6	;r8 = 4 * 10 = 40
-	sub r9, r12, r8	;r9 = 47 - 40 = 7
+	mul r8, r7, r6		;r8 = 4 * 10 = 40
+	sub r9, r12, r8		;r9 = 47 - 40 = 7
 	
-	mov r10, #0x30
+	mov r10, #0x30		;r10 = #0x30
 	add r11, r7, r10	;r11 = 0x04 + 0x30 = 0x34
 	add r12, r9, r10	;r12 = 0x07 + 0x30 = 0x37
 	
-	bx lr
+	bx lr				;branches back to main
 	
 	end
